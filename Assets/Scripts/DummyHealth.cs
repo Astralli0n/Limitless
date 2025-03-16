4aa9197f86236792b424b9a276afb15d7a3987f8
Astrallion <astrallionstudios@gmail.com>
2025-03-16 12:32:25 +0000

    Added dummy for testing purposes

--- Assets/Scripts/DummyHealth.cs
+++ Assets/Scripts/DummyHealth.cs
@@ -1,6 +1,8 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 public class DummyHealth : Health {
+    [SerializeField] Image HealthBar;
     protected override void Die() {
         gameObject.SetActive(false); // Disappear when "killed"
         Invoke(nameof(Respawn), 1f); // Respawn after 1 second
